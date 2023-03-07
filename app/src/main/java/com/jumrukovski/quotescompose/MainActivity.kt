a49194a629fd33d9b9593583f3c93c03b95d3d11
mendejumrukovski <mende@code.livelike.com>
2023-03-07 02:00:19 +0100

    Create ui shared package and move BottomNavigationBar and TopAppBar

--- app/src/main/java/com/jumrukovski/quotescompose/MainActivity.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/MainActivity.kt
@@ -8,97 +8,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.jumrukovski.quotescompose.ui.theme.*
+import com.jumrukovski.quotescompose.ui.shared.BottomNavigationBar
+import com.jumrukovski.quotescompose.ui.shared.Toolbar
+import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
+import com.jumrukovski.quotescompose.ui.theme.QuotesComposeTheme
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    private fun Toolbar(
-        topMenuItems: List<MainActivityMenuItem>,
-        onActionClick: (MainActivityMenuItem) -> Unit = {}
-    ) {
-        TopAppBar(
-            modifier = Modifier.background(MaterialTheme.colorScheme.PrimaryBackgroundColor),
-            title = { Text(stringResource(R.string.app_name)) },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.PrimaryBackgroundColor,
-                scrolledContainerColor = MaterialTheme.colorScheme.PrimaryBackgroundColor,
-                navigationIconContentColor = MaterialTheme.colorScheme.PrimaryTextColor,
-                titleContentColor = MaterialTheme.colorScheme.PrimaryTextColor,
-                actionIconContentColor = MaterialTheme.colorScheme.PrimaryTextColor
-            ),
-            actions = {
-                topMenuItems.forEach { topMenuItem ->
-                    TopMenuItem(topMenuItem = topMenuItem, onActionClick = { onActionClick(it) })
-                }
-            }
-        )
-    }
-
-    @Composable
-    private fun TopMenuItem(
-        topMenuItem: MainActivityMenuItem,
-        onActionClick: (MainActivityMenuItem) -> Unit = {}
-    ) {
-        IconButton(onClick = { onActionClick(topMenuItem) }) {
-            Icon(
-                painter = painterResource(id = topMenuItem.drawable),
-                contentDescription = topMenuItem.title,
-                tint = MaterialTheme.colorScheme.onSurface
-            )
-        }
-    }
-
     //todo do it differently
     data class MainActivityMenuItem(val title: String, @DrawableRes val drawable: Int)
-
-    @Composable
-    private fun BottomNavigationBar(onItemClick: (BottomMenuItem) -> Unit = {}) {
-        val selectedIndex = remember { mutableStateOf(0) }
-
-        NavigationBar(
-            modifier = Modifier,
-            contentColor = MaterialTheme.colorScheme.NavigationBarItemRippleColor,
-            tonalElevation = 0.dp,
-            containerColor = MaterialTheme.colorScheme.NavigationBarBackgroundColor,
-            content = {
-                BottomMenuItem.values().forEachIndexed { index, menuItem ->
-                    NavigationBarItem(
-                        selected = selectedIndex.value == index,
-                        onClick = {
-                            selectedIndex.value = index
-                            onItemClick(BottomMenuItem.values()[index])
-                        },
-                        label = { Text(text = stringResource(id = menuItem.titleTextId)) },
-                        enabled = true,
-                        icon = {
-                            Icon(
-                                painter = painterResource(id = menuItem.icon),
-                                contentDescription = stringResource(id = menuItem.titleTextId)
-                            )
-                        },
-                        alwaysShowLabel = true,
-                        colors = NavigationBarItemDefaults.colors(
-                            selectedIconColor = MaterialTheme.colorScheme.NavigationBarSelectedItemColor,
-                            unselectedIconColor = MediumDarkGreyColor,
-                            selectedTextColor = MaterialTheme.colorScheme.NavigationBarSelectedItemColor,
-                            unselectedTextColor = MediumDarkGreyColor,
-                            indicatorColor = MaterialTheme.colorScheme.NavigationBarBackgroundColor
-                        )
-                    )
-                }
-            }
-        )
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -114,7 +36,7 @@ class MainActivity : ComponentActivity() {
             QuotesComposeTheme {
                 Scaffold(
                     topBar = {
-                        Toolbar(topMenuItems) { menuItem ->
+                        Toolbar(stringResource(id = R.string.app_name), topMenuItems) { menuItem ->
                             //todo handle top menu item
                         }
                     },
---
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/shared/BottomNavigationBar.kt
@@ -0,0 +1,54 @@
+package com.jumrukovski.quotescompose.ui.shared
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.jumrukovski.quotescompose.BottomMenuItem
+import com.jumrukovski.quotescompose.ui.theme.MediumDarkGreyColor
+import com.jumrukovski.quotescompose.ui.theme.NavigationBarBackgroundColor
+import com.jumrukovski.quotescompose.ui.theme.NavigationBarItemRippleColor
+import com.jumrukovski.quotescompose.ui.theme.NavigationBarSelectedItemColor
+
+@Composable
+fun BottomNavigationBar(onItemClick: (BottomMenuItem) -> Unit = {}) {
+    val selectedIndex = remember { mutableStateOf(0) }
+
+    NavigationBar(
+        modifier = Modifier,
+        contentColor = MaterialTheme.colorScheme.NavigationBarItemRippleColor,
+        tonalElevation = 0.dp,
+        containerColor = MaterialTheme.colorScheme.NavigationBarBackgroundColor,
+        content = {
+            BottomMenuItem.values().forEachIndexed { index, menuItem ->
+                NavigationBarItem(
+                    selected = selectedIndex.value == index,
+                    onClick = {
+                        selectedIndex.value = index
+                        onItemClick(BottomMenuItem.values()[index])
+                    },
+                    label = { Text(text = stringResource(id = menuItem.titleTextId)) },
+                    enabled = true,
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = menuItem.icon),
+                            contentDescription = stringResource(id = menuItem.titleTextId)
+                        )
+                    },
+                    alwaysShowLabel = true,
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = MaterialTheme.colorScheme.NavigationBarSelectedItemColor,
+                        unselectedIconColor = MediumDarkGreyColor,
+                        selectedTextColor = MaterialTheme.colorScheme.NavigationBarSelectedItemColor,
+                        unselectedTextColor = MediumDarkGreyColor,
+                        indicatorColor = MaterialTheme.colorScheme.NavigationBarBackgroundColor
+                    )
+                )
+            }
+        }
+    )
+}
