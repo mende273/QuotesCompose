d38bc3ebaacb8316edca9ba148a28675811c1d45
mendejumrukovski <mende@code.livelike.com>
2023-03-11 01:57:36 +0100

    refactor screen packages

---
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/feature/categories/CategoriesScreen.kt
@@ -0,0 +1,22 @@
+package com.jumrukovski.quotescompose.ui.feature.categories
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
+
+@Composable
+fun CategoriesScreen(navHostController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.PrimaryBackgroundColor)
+    ) {
+        Text(text = "Categories screen")
+    }
+}
---
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/feature/favourites/FavouritesScreen.kt
@@ -0,0 +1,22 @@
+package com.jumrukovski.quotescompose.ui.feature.favourites
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
+
+@Composable
+fun FavouritesScreen(navHostController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.PrimaryBackgroundColor)
+    ) {
+        Text(text = "Favourites screen")
+    }
+}
