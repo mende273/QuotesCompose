1de2ccf5359c952305d3ed95575efb14bd2c7865
mendejumrukovski <mende@code.livelike.com>
2023-03-14 00:17:12 +0100

    Add dumb home header category items as a temporary placeholder

--- app/src/main/java/com/jumrukovski/quotescompose/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/feature/home/HomeScreen.kt
@@ -2,11 +2,16 @@ package com.jumrukovski.quotescompose.ui.feature.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
 
@@ -17,6 +22,20 @@ fun HomeScreen(navHostController: NavHostController) {
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.PrimaryBackgroundColor)
     ) {
-        Text(text = "Home screen")
+        MainScreen()
+    }
+}
+
+@Composable
+private fun MainScreen(){
+    val dumbItems = listOf("item 1","item 2","item 3","item 4")
+    Column {
+        LazyRow{
+            items(dumbItems){
+                Box(modifier = Modifier.padding(16.dp)){
+                    Text(text = it)
+                }
+            }
+        }
     }
 }
