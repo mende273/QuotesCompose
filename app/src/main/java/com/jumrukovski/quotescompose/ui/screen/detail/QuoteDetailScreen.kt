cd216c5ea78ff8374323ab92e003f4f6c57ddba5
mendejumrukovski <mende@code.livelike.com>
2023-03-18 00:38:58 +0100

    create quote detail screen

--- app/src/main/java/com/jumrukovski/quotescompose/ui/screen/detail/QuoteDetailScreen.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/screen/detail/QuoteDetailScreen.kt
@@ -8,16 +8,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.jumrukovski.quotescompose.data.model.QuoteDTO
 import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
 
 @Composable
-fun QuoteDetailScreen(){
+fun QuoteDetailScreen(quoteDTO: QuoteDTO?){
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.PrimaryBackgroundColor),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Quote detail Screen")
+        quoteDTO?.let {
+            Text(text = quoteDTO.content)
+        }
     }
 }
