6d08a055604706decd9fbdd4924b9a25bb2b4e06
mendejumrukovski <mende@code.livelike.com>
2023-03-25 00:09:02 +0100

    update tags screen

--- app/src/main/java/com/jumrukovski/quotescompose/ui/screen/tags/TagsScreen.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/screen/tags/TagsScreen.kt
@@ -3,24 +3,27 @@ package com.jumrukovski.quotescompose.ui.screen.tags
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jumrukovski.quotescompose.R
 import com.jumrukovski.quotescompose.data.model.TagDTO
 import com.jumrukovski.quotescompose.ui.common.Toolbar
 import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
+import com.jumrukovski.quotescompose.ui.theme.PrimaryTextColor
 import com.jumrukovski.quotescompose.ui.theme.QuotesComposeTheme
+import com.jumrukovski.quotescompose.ui.theme.TertiaryColor
 
 @Composable
 fun TagsScreen(
@@ -53,23 +56,45 @@ private fun Contents(
     tagItems: List<TagDTO>,
     onNavigateToSelectedTag: (String) -> Unit
 ) {
-
     Box(
         modifier = Modifier
             .padding(paddingValues)
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.PrimaryBackgroundColor)
     ) {
-        LazyColumn {
+        LazyVerticalGrid(
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            columns = GridCells.Adaptive(minSize = 150.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
             items(tagItems) {
-                Text(
+                Card(
                     modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .background(color = MaterialTheme.colorScheme.PrimaryBackgroundColor)
                         .fillMaxWidth()
-                        .padding(16.dp)
                         .clickable {
                             onNavigateToSelectedTag(it.name)
-                        }, text = it.name, color = Color.Black
-                )
+                        },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.TertiaryColor,
+                    )
+                ) {
+                    Box(modifier = Modifier.padding(16.dp)) {
+                        Text(
+                            modifier = Modifier
+                                .wrapContentSize(),
+                            text = it.name,
+                            style = TextStyle(
+                                color = MaterialTheme.colorScheme.PrimaryTextColor,
+                                fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
+                                fontSize = 20.sp
+                            )
+                        )
+                    }
+                }
             }
         }
     }
