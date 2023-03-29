25c9c8213ccfdd04fe9786be59b8882424837cd1
mendejumrukovski <mende@code.livelike.com>
2023-03-30 01:49:53 +0200

    create LargeQuoteCard.kt component

---
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/common/component/LargeQuoteCard.kt
@@ -0,0 +1,62 @@
+package com.jumrukovski.quotescompose.ui.common.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jumrukovski.quotescompose.data.model.QuoteDTO
+import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
+import com.jumrukovski.quotescompose.ui.theme.PrimaryTextColor
+import com.jumrukovski.quotescompose.ui.theme.TertiaryColor
+
+@Composable
+fun LargeQuoteCard(quoteDTO: QuoteDTO) {
+    Card(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.PrimaryBackgroundColor)
+            .padding(16.dp)
+            .fillMaxSize(),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.TertiaryColor,
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column {
+                Text(
+                    modifier = Modifier
+                        .wrapContentHeight(),
+                    text = quoteDTO.content,
+                    style = TextStyle(
+                        color = MaterialTheme.colorScheme.PrimaryTextColor,
+                        fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
+                        fontSize = 30.sp
+                    )
+                )
+                Text(
+                    modifier = Modifier
+                        .wrapContentHeight(),
+                    text = quoteDTO.author,
+                    style = TextStyle(
+                        color = MaterialTheme.colorScheme.PrimaryTextColor,
+                        fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/jumrukovski/quotescompose/ui/screen/random/RandomQuoteScreen.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/screen/random/RandomQuoteScreen.kt
@@ -1,34 +1,30 @@
 package com.jumrukovski.quotescompose.ui.screen.random
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.jumrukovski.quotescompose.R
 import com.jumrukovski.quotescompose.data.model.QuoteDTO
 import com.jumrukovski.quotescompose.ui.common.Toolbar
+import com.jumrukovski.quotescompose.ui.common.component.LargeQuoteCard
 import com.jumrukovski.quotescompose.ui.common.component.ProgressBar
 import com.jumrukovski.quotescompose.ui.common.state.UIState
-import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
-import com.jumrukovski.quotescompose.ui.theme.PrimaryTextColor
 import com.jumrukovski.quotescompose.ui.theme.QuotesComposeTheme
-import com.jumrukovski.quotescompose.ui.theme.TertiaryColor
 
 @Composable
 fun RandomQuoteScreen(viewModel: RandomQuoteViewModel, onNavigateBack: () -> Unit) {
 
     LaunchedEffect(key1 = "random_quote",
         block = {
-        viewModel.getRandomQuote()
-    })
+            viewModel.getRandomQuote()
+        })
 
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
 
@@ -54,53 +50,12 @@ private fun Contents(paddingValues: PaddingValues, uiState: UIState<QuoteDTO>) {
             .padding(paddingValues)
             .fillMaxSize()
     ) {
-        when(uiState){
+        when (uiState) {
             is UIState.Error -> ""
             is UIState.Exception -> ""
             UIState.Loading -> ProgressBar()
             UIState.SuccessWithNoData -> ""
-            is UIState.SuccessWithData -> {
-                Card(
-                    modifier = Modifier
-                        .background(color = MaterialTheme.colorScheme.PrimaryBackgroundColor)
-                        .padding(16.dp)
-                        .fillMaxSize(),
-                    shape = MaterialTheme.shapes.medium,
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.TertiaryColor,
-                    )
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Column {
-                            Text(
-                                modifier = Modifier
-                                    .wrapContentHeight(),
-                                text = uiState.data.content,
-                                style = TextStyle(
-                                    color = MaterialTheme.colorScheme.PrimaryTextColor,
-                                    fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
-                                    fontSize = 30.sp
-                                )
-                            )
-                            Text(
-                                modifier = Modifier
-                                    .wrapContentHeight(),
-                                text = uiState.data.author,
-                                style = TextStyle(
-                                    color = MaterialTheme.colorScheme.PrimaryTextColor,
-                                    fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
-                                    fontSize = 20.sp
-                                )
-                            )
-                        }
-                    }
-                }
-            }
+            is UIState.SuccessWithData -> LargeQuoteCard(quoteDTO = uiState.data)
         }
     }
 }
