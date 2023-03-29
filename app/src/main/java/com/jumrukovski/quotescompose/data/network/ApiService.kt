930c0fb522393d0f69feed3015db30f5b13e5eea
mendejumrukovski <mende@code.livelike.com>
2023-03-30 01:21:57 +0200

    add random quotes request in ApiService.kt and Repository.kt

--- app/src/main/java/com/jumrukovski/quotescompose/data/network/ApiService.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/data/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.jumrukovski.quotescompose.data.network
 
+import com.jumrukovski.quotescompose.data.model.QuoteDTO
 import com.jumrukovski.quotescompose.data.model.QuotesResultsDTO
 import com.jumrukovski.quotescompose.data.model.TagDTO
 import retrofit2.Response
@@ -13,4 +14,7 @@ interface ApiService {
 
     @GET("quotes")
     suspend fun getQuotesForTag(@Query("tags") tag: String): Response<QuotesResultsDTO>
+
+    @GET("random")
+    suspend fun getRandomQuote(): Response<QuoteDTO>
 }
