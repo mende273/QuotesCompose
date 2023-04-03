103daea1d29de06ca1d2bb681728f8a17e8187d6
mendejumrukovski <mende@code.livelike.com>
2023-04-04 01:19:48 +0200

    improve LargeQuoteCard.kt

--- app/src/main/java/com/jumrukovski/quotescompose/ui/common/component/LargeQuoteCard.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/common/component/LargeQuoteCard.kt
@@ -7,55 +7,43 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
-import com.jumrukovski.quotescompose.ui.theme.PrimaryTextColor
 import com.jumrukovski.quotescompose.ui.theme.TertiaryColor
+import com.jumrukovski.quotescompose.ui.theme.largeTextStyle
+import com.jumrukovski.quotescompose.ui.theme.mediumTextStyle
 
 @Composable
 fun LargeQuoteCard(content: String, author: String) {
     Card(
         modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.PrimaryBackgroundColor)
+            .fillMaxSize()
             .padding(16.dp)
-            .fillMaxSize(),
+            .background(color = MaterialTheme.colorScheme.PrimaryBackgroundColor),
         shape = MaterialTheme.shapes.medium,
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.TertiaryColor,
         )
     ) {
-        Box(
+        Column(
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxSize(),
-            contentAlignment = Alignment.Center
+            verticalArrangement = Arrangement.Center
         ) {
-            Column {
-                Text(
-                    modifier = Modifier
-                        .wrapContentHeight(),
-                    text = content,
-                    style = TextStyle(
-                        color = MaterialTheme.colorScheme.PrimaryTextColor,
-                        fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
-                        fontSize = 30.sp
-                    )
-                )
-                Text(
-                    modifier = Modifier
-                        .wrapContentHeight(),
-                    text = author,
-                    style = TextStyle(
-                        color = MaterialTheme.colorScheme.PrimaryTextColor,
-                        fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
-                        fontSize = 20.sp
-                    )
-                )
-            }
+            Text(
+                modifier = Modifier
+                    .wrapContentHeight(),
+                text = content,
+                style = largeTextStyle()
+            )
+            Text(
+                modifier = Modifier
+                    .wrapContentHeight(),
+                text = author,
+                style = mediumTextStyle()
+            )
         }
     }
 }
