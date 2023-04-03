2bedc391a488e0e74ee3a1c880cfdc1f7c525c72
mendejumrukovski <mende@code.livelike.com>
2023-04-04 01:32:59 +0200

    add TextAlign.Start to largeTextStyle and mediumTextStyle

--- app/src/main/java/com/jumrukovski/quotescompose/ui/theme/Style.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/theme/Style.kt
@@ -3,6 +3,7 @@ package com.jumrukovski.quotescompose.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -10,7 +11,8 @@ fun largeTextStyle(): TextStyle {
     return TextStyle(
         color = MaterialTheme.colorScheme.PrimaryTextColor,
         fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
-        fontSize = 30.sp
+        fontSize = 30.sp,
+        textAlign = TextAlign.Start
     )
 }
 
@@ -19,6 +21,7 @@ fun mediumTextStyle(): TextStyle {
     return TextStyle(
         color = MaterialTheme.colorScheme.PrimaryTextColor,
         fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
-        fontSize = 20.sp
+        fontSize = 20.sp,
+        textAlign = TextAlign.Start
     )
 }
