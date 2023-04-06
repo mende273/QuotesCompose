2900574254a25e2acb503add7c2508c460a696e6
mendejumrukovski <mende@code.livelike.com>
2023-04-07 01:33:45 +0200

    refactor MenuItem.kt

--- app/src/main/java/com/jumrukovski/quotescompose/data/model/MenuItem.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/data/model/MenuItem.kt
@@ -3,4 +3,9 @@ package com.jumrukovski.quotescompose.data.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class MenuItem(@StringRes val titleTextId: Int, @DrawableRes val icon: Int)
+data class MenuItem(
+    @StringRes val titleTextId: Int,
+    @DrawableRes val icon: Int,
+    @DrawableRes val selectedIcon: Int,
+    var isSelected: Boolean
+)
