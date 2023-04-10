cfe9e499d1656f6286e7aefd3fb91d2f9c892cb7
mendejumrukovski <mende@code.livelike.com>
2023-04-10 16:15:38 +0200

    remove parcelable implementation from QuoteDTO.kt

--- app/src/main/java/com/jumrukovski/quotescompose/data/model/dto/QuoteDTO.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/data/model/dto/QuoteDTO.kt
@@ -1,8 +1,5 @@
 package com.jumrukovski.quotescompose.data.model.dto
 
-import android.os.Parcel
-import android.os.Parcelable
-
 data class QuoteDTO(
     val _id: String,
     val author: String,
@@ -12,41 +9,4 @@ data class QuoteDTO(
     val length: Long,
     val dateAdded: String,
     val dateModified: String
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.createStringArrayList() ?: emptyList(),
-        parcel.readString() ?: "",
-        parcel.readLong(),
-        parcel.readString() ?: "",
-        parcel.readString() ?: ""
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(_id)
-        parcel.writeString(author)
-        parcel.writeString(content)
-        parcel.writeStringList(tags)
-        parcel.writeString(authorSlug)
-        parcel.writeLong(length)
-        parcel.writeString(dateAdded)
-        parcel.writeString(dateModified)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<QuoteDTO> {
-        override fun createFromParcel(parcel: Parcel): QuoteDTO {
-            return QuoteDTO(parcel)
-        }
-
-        override fun newArray(size: Int): Array<QuoteDTO?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
