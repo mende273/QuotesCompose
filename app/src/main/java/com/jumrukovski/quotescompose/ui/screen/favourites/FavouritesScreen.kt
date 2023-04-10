33fa0f38f2746f5dda724f1204ffa58687364ff1
mendejumrukovski <mende@code.livelike.com>
2023-04-11 01:36:20 +0200

    refactor FavouritesScreen.kt

--- app/src/main/java/com/jumrukovski/quotescompose/ui/screen/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/screen/favourites/FavouritesScreen.kt
@@ -1,47 +1,92 @@
 package com.jumrukovski.quotescompose.ui.screen.favourites
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavHostController
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.jumrukovski.quotescompose.R
+import com.jumrukovski.quotescompose.data.model.entity.FavouriteQuoteEntity
+import com.jumrukovski.quotescompose.data.model.middleware.Quote
 import com.jumrukovski.quotescompose.ui.common.TopBar
+import com.jumrukovski.quotescompose.ui.common.component.ProgressBar
+import com.jumrukovski.quotescompose.ui.common.component.SmallQuoteCard
+import com.jumrukovski.quotescompose.ui.common.state.UIState
 import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
 import com.jumrukovski.quotescompose.ui.theme.QuotesComposeTheme
 
 @Composable
-fun FavouritesScreen(navHostController: NavHostController) {
+fun FavouritesScreen(
+    viewModel: FavouritesViewModel,
+    onNavigateToQuoteDetails: (Quote) -> Unit,
+    onNavigateBack: () -> Unit
+) {
+
+    val favouriteItems by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.getAllFavourites()
+    }
+
     QuotesComposeTheme {
         Scaffold(
             topBar = {
                 TopBar(
-                    stringResource(id = R.string.screen_favourites)
+                    title = stringResource(id = R.string.screen_favourites),
+                    isBackButtonEnabled = true,
+                    onNavigateBack = onNavigateBack
                 )
             },
             content = { paddingValues ->
-                Contents(paddingValues = paddingValues)
+                Contents(
+                    paddingValues = paddingValues,
+                    state = favouriteItems,
+                    onNavigateToQuoteDetails = {
+                        onNavigateToQuoteDetails(it)
+                    })
             })
     }
 }
 
 @Composable
-private fun Contents(paddingValues: PaddingValues) {
+private fun Contents(
+    paddingValues: PaddingValues,
+    state: UIState<List<FavouriteQuoteEntity>>,
+    onNavigateToQuoteDetails: (Quote) -> Unit
+) {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(paddingValues)
-            .background(MaterialTheme.colorScheme.PrimaryBackgroundColor),
-        contentAlignment = Alignment.Center
+            .background(MaterialTheme.colorScheme.PrimaryBackgroundColor)
+            .padding(16.dp)
     ) {
-        Text(text = "Favourites screen")
+        when (state) {
+            is UIState.Error -> ""
+            is UIState.Exception -> ""
+            is UIState.Loading -> {
+                ProgressBar()
+            }
+            UIState.SuccessWithNoData -> ""
+            is UIState.SuccessWithData -> {
+                LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                    items(state.data) { quote ->
+                        SmallQuoteCard(
+                            quote = Quote(quote.id, quote.content, quote.author),
+                            onNavigateToQuoteDetails = {
+                                onNavigateToQuoteDetails(it)
+                            })
+                    }
+                }
+            }
+        }
     }
 }
