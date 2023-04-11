832ff9cc1214d993f0cd3e532bc5426d5ac0491d
mendejumrukovski <mende@code.livelike.com>
2023-04-12 00:54:22 +0200

    refactor TagsViewModel.kt

--- app/src/main/java/com/jumrukovski/quotescompose/ui/screen/tags/TagsViewModel.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/screen/tags/TagsViewModel.kt
@@ -2,21 +2,20 @@ package com.jumrukovski.quotescompose.ui.screen.tags
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jumrukovski.quotescompose.data.model.dto.TagDTO
 import com.jumrukovski.quotescompose.data.model.middleware.Tag
-import com.jumrukovski.quotescompose.data.repository.Repository
+import com.jumrukovski.quotescompose.data.network.ResponseResult
+import com.jumrukovski.quotescompose.domain.usecase.GetAllTagsUseCase
 import com.jumrukovski.quotescompose.ui.common.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
-class TagsViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
-    private val _uiState: MutableStateFlow<UIState<List<Tag>>> =
-        MutableStateFlow(UIState.Loading)
+class TagsViewModel @Inject constructor(private val getAllTagsUseCase: GetAllTagsUseCase) :
+    ViewModel() {
+    private val _uiState: MutableStateFlow<UIState<List<Tag>>> = MutableStateFlow(UIState.Loading)
     val uiState: StateFlow<UIState<List<Tag>>> = _uiState
 
     suspend fun getAllTags() {
@@ -25,24 +24,17 @@ class TagsViewModel @Inject constructor(private val repository: Repository) : Vi
                 return@launch
             }
 
-            _uiState.value = try {
-                val response: Response<List<TagDTO>> = repository.getAllTags()
-                when (response.isSuccessful) {
-                    true -> {
-                        response.body()?.let {
-                            if (it.isNotEmpty()) {
-                                UIState.SuccessWithData(it.map { Tag(it._id, it.name) })
-                            } else {
-                                UIState.SuccessWithNoData
-                            }
-                        } ?: UIState.SuccessWithNoData
-                    }
-                    false -> {
-                        UIState.Error(response.code())
+            val response: ResponseResult<List<Tag>> = getAllTagsUseCase()
+
+            _uiState.value = when (response) {
+                is ResponseResult.Error -> UIState.Error(response.code)
+                is ResponseResult.Exception -> UIState.Exception(response.exception)
+                is ResponseResult.Success -> {
+                    when (response.data.isEmpty()) {
+                        true -> UIState.SuccessWithNoData
+                        false -> UIState.SuccessWithData(response.data)
                     }
                 }
-            } catch (e: java.lang.Exception) {
-                UIState.Exception(e)
             }
         }
     }
