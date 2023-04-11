be2b309985b04c8882d21d09dff743881ae34d6d
mendejumrukovski <mende@code.livelike.com>
2023-04-12 01:24:18 +0200

    refactor ResponseMapper.kt

--- app/src/main/java/com/jumrukovski/quotescompose/domain/mapper/ResponseMapper.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/domain/mapper/ResponseMapper.kt
@@ -1,11 +1,14 @@
 package com.jumrukovski.quotescompose.domain.mapper
 
+import com.jumrukovski.quotescompose.data.model.dto.QuoteDTO
+import com.jumrukovski.quotescompose.data.model.dto.QuotesResultsDTO
 import com.jumrukovski.quotescompose.data.model.dto.TagDTO
+import com.jumrukovski.quotescompose.data.model.middleware.Quote
 import com.jumrukovski.quotescompose.data.model.middleware.Tag
 import com.jumrukovski.quotescompose.data.network.ResponseResult
 import retrofit2.Response
 
-fun Response<List<TagDTO>>.wrapAsResponseResult(): ResponseResult<List<Tag>> {
+fun Response<List<TagDTO>>.wrapTagsAsResponseResult(): ResponseResult<List<Tag>> {
     return try {
         return when (this.isSuccessful) {
             true -> ResponseResult.Success(this.body()?.mapToTags() ?: emptyList())
@@ -16,4 +19,19 @@ fun Response<List<TagDTO>>.wrapAsResponseResult(): ResponseResult<List<Tag>> {
     }
 }
 
-fun List<TagDTO>.mapToTags(): List<Tag> = this.map { Tag(it._id, it.name) }
+fun Response<QuotesResultsDTO>.wrapQuotesAsResponseResult(): ResponseResult<List<Quote>> {
+    return try {
+        return when (this.isSuccessful) {
+            true -> ResponseResult.Success(this.body()?.results?.mapToQuotes() ?: emptyList())
+            false -> ResponseResult.Error(this.errorBody(), this.code())
+        }
+    } catch (throwable: Throwable) {
+        ResponseResult.Exception(throwable)
+    }
+}
+
+private fun List<TagDTO>.mapToTags(): List<Tag> = this.map { Tag(id = it._id, name = it.name) }
+
+private fun List<QuoteDTO>.mapToQuotes(): List<Quote> = this.map {
+    Quote(id = it._id, content = it.content, author = it.author)
+}
--- app/src/main/java/com/jumrukovski/quotescompose/domain/usecase/GetAllTagsUseCase.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/domain/usecase/GetAllTagsUseCase.kt
@@ -1,9 +1,9 @@
 package com.jumrukovski.quotescompose.domain.usecase
 
 import com.jumrukovski.quotescompose.data.repository.RemoteRepository
-import com.jumrukovski.quotescompose.domain.mapper.wrapAsResponseResult
+import com.jumrukovski.quotescompose.domain.mapper.wrapTagsAsResponseResult
 
 class GetAllTagsUseCase(private val remoteRepository: RemoteRepository) {
 
-    suspend operator fun invoke() = remoteRepository.getAllTags().wrapAsResponseResult()
+    suspend operator fun invoke() = remoteRepository.getAllTags().wrapTagsAsResponseResult()
 }
