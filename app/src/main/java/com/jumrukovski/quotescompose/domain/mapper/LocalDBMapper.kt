aa7eee47e9d406f6a7bb6874c6c3aa1ee48c4005
mendejumrukovski <mende@code.livelike.com>
2023-04-13 17:51:00 +0200

    create LocalDBMapper.kt

---
+++ app/src/main/java/com/jumrukovski/quotescompose/domain/mapper/LocalDBMapper.kt
@@ -0,0 +1,18 @@
+package com.jumrukovski.quotescompose.domain.mapper
+
+import com.jumrukovski.quotescompose.data.model.entity.FavouriteQuoteEntity
+import com.jumrukovski.quotescompose.data.model.middleware.Quote
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+fun Flow<List<FavouriteQuoteEntity>>.mapToQuotes(): Flow<List<Quote>> {
+    return this.map { items ->
+        items.map {
+            Quote(id = it.id, content = it.content, author = it.author)
+        }
+    }
+}
+
+fun mapToFavouriteQuoteEntity(id: String, content: String, author: String): FavouriteQuoteEntity {
+    return FavouriteQuoteEntity(id, content, author)
+}
