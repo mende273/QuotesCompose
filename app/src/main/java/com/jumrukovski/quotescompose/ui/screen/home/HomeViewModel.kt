67b8512644f65ef4d78251e8e7070c86c1566027
mendejumrukovski <mende@code.livelike.com>
2023-04-14 00:47:40 +0200

    refactor HomeViewModel.kt

--- app/src/main/java/com/jumrukovski/quotescompose/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/screen/home/HomeViewModel.kt
@@ -3,7 +3,8 @@ package com.jumrukovski.quotescompose.ui.screen.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jumrukovski.quotescompose.data.model.middleware.Quote
-import com.jumrukovski.quotescompose.data.repository.RemoteRepository
+import com.jumrukovski.quotescompose.data.network.ResponseResult
+import com.jumrukovski.quotescompose.domain.usecase.GetQuotesUseCase
 import com.jumrukovski.quotescompose.ui.common.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,33 +13,27 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val remoteRepository:RemoteRepository):ViewModel(){
-    private val _uiState:MutableStateFlow<UIState<List<Quote>>> = MutableStateFlow(UIState.Loading)
-    val uiState:StateFlow<UIState<List<Quote>>> = _uiState
+class HomeViewModel @Inject constructor(private val getQuotesUseCase: GetQuotesUseCase) :
+    ViewModel() {
+    private val _uiState: MutableStateFlow<UIState<List<Quote>>> = MutableStateFlow(UIState.Loading)
+    val uiState: StateFlow<UIState<List<Quote>>> = _uiState
 
     // todo get paginated quotes list
-    suspend fun getQuotes(){
+    suspend fun getQuotes() {
         viewModelScope.launch {
             if (_uiState.value is UIState.SuccessWithData) {
                 return@launch
             }
 
-            _uiState.value = try {
-                val response = remoteRepository.getQuotes()
-                when (response.isSuccessful) {
-                    true -> {
-                        response.body()?.let {
-                            if (it.results.isNullOrEmpty()) {
-                                UIState.SuccessWithNoData
-                            } else {
-                                UIState.SuccessWithData(it.results.map { Quote(it._id,it.content,it.author) })
-                            }
-                        } ?: UIState.SuccessWithNoData
+            with(getQuotesUseCase()) {
+                _uiState.value = when (this) {
+                    is ResponseResult.Error -> UIState.Error(code)
+                    is ResponseResult.Exception -> UIState.Exception(exception)
+                    is ResponseResult.Success -> when (data.isEmpty()) {
+                        true -> UIState.SuccessWithNoData
+                        false -> UIState.SuccessWithData(data)
                     }
-                    false -> UIState.Error(response.code())
                 }
-            } catch (e: java.lang.Exception) {
-                UIState.Exception(e)
             }
         }
     }
