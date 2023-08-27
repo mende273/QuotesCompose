726c821e998e7b8d4d4a1cde741df7fcc243bdee
mendejumrukovski <mende@code.livelike.com>
2023-08-27 14:17:33 +0200

    update gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,10 @@ android {
         kotlinCompilerExtensionVersion = "1.4.8"
     }
 
-    packagingOptions {
-        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
@@ -84,12 +86,14 @@ dependencies {
 
     // compose
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha05")
-    implementation("androidx.compose.ui:ui:1.6.0-alpha03")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0-alpha03")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0-alpha03")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.0-alpha03")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha03")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha06")
+    platform("androidx.compose:compose-bom:2023.08.00")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // compose extensions
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
@@ -101,7 +105,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
 
     // navigation component
-    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("androidx.navigation:navigation-compose:2.7.1")
 
     // dagger Hilt dependency injection
     implementation("com.google.dagger:hilt-android:2.46.1")
