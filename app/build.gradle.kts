b5e22ed055151e524d491ea38a98130ee2198402
mendejumrukovski <mende@code.livelike.com>
2023-10-01 00:36:02 +0200

    update compose-bom to 2023.09.02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     // compose
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3:1.2.0-alpha08")
-    platform("androidx.compose:compose-bom:2023.09.01")
+    platform("androidx.compose:compose-bom:2023.09.02")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
