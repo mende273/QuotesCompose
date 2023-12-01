1062943cdfe7e0cc61833ed80449012b19449b8e
mendejumrukovski <mende@code.livelike.com>
2023-12-02 00:39:19 +0100

    refactor ProgressBar

--- app/src/main/java/com/jumrukovski/quotescompose/ui/common/component/ProgressBar.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/common/component/ProgressBar.kt
@@ -1,29 +1,16 @@
 package com.jumrukovski.quotescompose.ui.common.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
 import com.jumrukovski.quotescompose.ui.theme.PrimaryTextColor
 import com.jumrukovski.quotescompose.ui.theme.TertiaryColor
 
 @Composable
 @Preview
 fun ProgressBar() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(PrimaryBackgroundColor),
-        contentAlignment = Alignment.Center
-    ) {
-        CircularProgressIndicator(
-            color = TertiaryColor,
-            trackColor = PrimaryTextColor
-        )
-    }
+    CircularProgressIndicator(
+        color = TertiaryColor,
+        trackColor = PrimaryTextColor
+    )
 }
