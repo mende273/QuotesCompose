2f299d3b202f0af3549d63fb2514a023a1121547
mendejumrukovski <mende@code.livelike.com>
2023-12-04 00:03:24 +0100

    create TagPreviewParameter

--- app/src/main/java/com/jumrukovski/quotescompose/ui/common/component/TagCard.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/common/component/TagCard.kt
@@ -13,11 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.jumrukovski.quotescompose.domain.model.Tag
+import com.jumrukovski.quotescompose.ui.preview.parameter.TagPreviewParameter
 import com.jumrukovski.quotescompose.ui.theme.PrimaryBackgroundColor
 import com.jumrukovski.quotescompose.ui.theme.TertiaryColor
 import com.jumrukovski.quotescompose.ui.theme.mediumTextStyle
+import com.jumrukovski.quotescompose.ui.theme.spacing
 
 @Composable
 fun TagCard(tag: Tag, onItemClicked: (String) -> Unit) {
@@ -36,7 +38,7 @@ fun TagCard(tag: Tag, onItemClicked: (String) -> Unit) {
         Text(
             modifier = Modifier
                 .wrapContentSize()
-                .padding(16.dp),
+                .padding(MaterialTheme.spacing.normal),
             text = tag.name,
             style = mediumTextStyle()
         )
@@ -45,12 +47,16 @@ fun TagCard(tag: Tag, onItemClicked: (String) -> Unit) {
 
 @Preview
 @Composable
-private fun TagCardPreview() {
-    TagCard(tag = Tag("123", "tag name"), onItemClicked = {})
+private fun TagCardPreview(
+    @PreviewParameter(TagPreviewParameter::class) tag: Tag
+) {
+    TagCard(tag = tag, onItemClicked = {})
 }
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
-private fun TagCardDarkPreview() {
-    TagCard(tag = Tag("123", "tag name"), onItemClicked = {})
+private fun TagCardDarkPreview(
+    @PreviewParameter(TagPreviewParameter::class) tag: Tag
+) {
+    TagCard(tag = tag, onItemClicked = {})
 }
