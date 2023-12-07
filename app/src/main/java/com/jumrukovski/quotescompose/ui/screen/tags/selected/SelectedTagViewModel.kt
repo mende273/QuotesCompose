41cbd4311819935846025cfd3548d8cbe1b601e3
mendejumrukovski <mende@code.livelike.com>
2023-12-07 23:48:56 +0100

    refactor SelectedTagViewModel

--- app/src/main/java/com/jumrukovski/quotescompose/ui/screen/tags/selected/SelectedTagViewModel.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/screen/tags/selected/SelectedTagViewModel.kt
@@ -3,7 +3,6 @@ package com.jumrukovski.quotescompose.ui.screen.tags.selected
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jumrukovski.quotescompose.domain.mapper.toUiState
 import com.jumrukovski.quotescompose.domain.model.Quote
 import com.jumrukovski.quotescompose.domain.repository.RemoteRepositoryImpl
 import com.jumrukovski.quotescompose.ui.common.state.UIState
@@ -22,7 +21,6 @@ class SelectedTagViewModel @Inject constructor(
 
     companion object {
         private const val TAG_KEY = "tag"
-        private const val TAG_ITEMS_KEY = "tagItems"
     }
 
     private val _uiState: MutableStateFlow<UIState<List<Quote>>> = MutableStateFlow(UIState.Loading)
@@ -34,13 +32,18 @@ class SelectedTagViewModel @Inject constructor(
                 if (tag != this.get<String>(TAG_KEY)) {
                     this[TAG_KEY] = tag
 
-                    this[TAG_ITEMS_KEY] = remoteRepository.getQuotesForTag(tag).fold(
-                        onSuccess = { it },
-                        onFailure = { null }
+                    _uiState.value = remoteRepository.getQuotesForTag(tag).fold(
+                        onSuccess = {
+                            when (it.isEmpty()) {
+                                true -> UIState.SuccessWithNoData
+                                false -> UIState.SuccessWithData(it)
+                            }
+                        },
+                        onFailure = {
+                            UIState.ErrorRetrievingData
+                        }
                     )
                 }
-
-                _uiState.value = this.get<List<Quote>>(TAG_ITEMS_KEY).toUiState()
             }
         }
     }
