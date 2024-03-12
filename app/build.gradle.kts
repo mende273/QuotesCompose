74a873fb156bac33d7e3a96b6da351860e6be041
mende273 <mende_273@hotmail.com>
2024-03-12 02:33:14 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,8 +93,8 @@ dependencies {
 
     // compose
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material3:material3:1.2.0-beta01")
-    platform("androidx.compose:compose-bom:2023.10.01")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    platform("androidx.compose:compose-bom:2024.02.01")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -107,17 +107,17 @@ dependencies {
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 
     // lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     // navigation component
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // dagger Hilt dependency injection
-    implementation("com.google.dagger:hilt-android:2.48")
-    ksp("com.google.dagger:hilt-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:2.49")
+    ksp("com.google.dagger:hilt-compiler:2.49")
 
     // Activity KTX for viewModels()
     implementation("androidx.activity:activity-ktx:1.8.2")
