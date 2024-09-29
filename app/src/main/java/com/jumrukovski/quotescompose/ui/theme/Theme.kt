ddcfcf31a5a14d9360b8421c0f5159c0628251c5
mende273 <mende_273@hotmail.com>
2024-09-30 00:48:51 +0200

    fix theme

--- app/src/main/java/com/jumrukovski/quotescompose/ui/theme/Theme.kt
+++ app/src/main/java/com/jumrukovski/quotescompose/ui/theme/Theme.kt
@@ -1,29 +1,28 @@
 package com.jumrukovski.quotescompose.ui.theme
 
-import android.os.Build
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.core.view.WindowCompat
 
 private val darkColorScheme = darkColorScheme(
     primary = BlackColor,
     secondary = GreyColor,
-    tertiary = TertiaryColorDarkTheme
+    tertiary = TertiaryColorDarkTheme,
+    background = BlackColor
 )
 
 private val lightColorScheme = lightColorScheme(
     primary = GreyColor,
     secondary = BlackColor,
-    tertiary = TertiaryColorLightTheme
+    tertiary = TertiaryColorLightTheme,
+    background = GreyColor
 )
 
 val TertiaryColor: Color
@@ -57,28 +56,19 @@ val NavigationBarItemRippleColor: Color
 @Composable
 fun QuotesComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> darkColorScheme
         else -> lightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
-        val systemUiController = rememberSystemUiController()
-
         SideEffect {
-            with(systemUiController) {
-                setStatusBarColor(color = if (darkTheme) BlackColor else GreyColor)
-                setNavigationBarColor(color = if (darkTheme) DarkGreyColor else WhiteColor)
-            }
+            val window = (view.context as Activity).window
+
+            val windowsInsetsController = WindowCompat.getInsetsController(window, view)
+            windowsInsetsController.isAppearanceLightStatusBars = !darkTheme
         }
     }
 
